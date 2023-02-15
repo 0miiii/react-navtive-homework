8a96d1dd87374015857a1ad8378f91118718a77b
mini <0miiii@naver.com>
2023-02-15 15:42:09 +0900

    feat: bottom navigation 생성

--- App.tsx
+++ App.tsx
@@ -1,20 +1,91 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import { FontAwesome5 } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-export default function App() {
+const Tab = createBottomTabNavigator();
+
+function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text>테스트123</Text>
-      <StatusBar style="auto" />
+      <Text>Home!</Text>
     </View>
   );
 }
 
+function CalendarScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>CalendarScreen!</Text>
+    </View>
+  );
+}
+
+function LibraryScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>LibraryScreen!</Text>
+    </View>
+  );
+}
+
+function MypageScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>MypageScreen!</Text>
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{ headerShown: false, tabBarActiveTintColor: "black" }}
+        backBehavior="history"
+      >
+        <Tab.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="CALENDAR"
+          component={CalendarScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="calendar" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="LIBRARY"
+          component={LibraryScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="dumbbell" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="MY PAGE"
+          component={MypageScreen}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <FontAwesome5 name="user" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
 });
