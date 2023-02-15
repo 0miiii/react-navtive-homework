af29e07ee1c2c315982e581173296e72d199ba7a
mini <0miiii@naver.com>
2023-02-15 18:07:16 +0900

    feat: create calendarNav

--- components/Calendar.tsx
+++ components/Calendar.tsx
@@ -1,15 +1,33 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 function Calendar() {
+  const currentYear = new Date().getFullYear();
+  const currentMonth = new Date().getMonth() + 1;
+
   return (
     <View style={styles.container}>
-      <Text>캘린더</Text>
+      <View style={styles.calendarNav}>
+        <Button title="<" />
+        <Text>{`${currentMonth} ${currentYear}`}</Text>
+        <Button title=">" />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  calendarNav: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    borderWidth: 1,
+  },
 });
 
 export default Calendar;
