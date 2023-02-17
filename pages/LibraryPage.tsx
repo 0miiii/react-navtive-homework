695788a1dbb0a8ba7d88c9a61eb91622bf9bbe76
yeongmin <0miiii@naver.com>
2023-02-17 17:06:52 +0900

    feat: delete calendar being test and change it to origin page

--- pages/LibraryPage.tsx
+++ pages/LibraryPage.tsx
@@ -1,7 +1,15 @@
-import WeeklyCalendar from "../components/WeeklyCalendar";
+import { StyleSheet, Text, View } from "react-native";
 
 function LibraryScreen() {
-  return <WeeklyCalendar />;
+  return (
+    <View style={styles.container}>
+      <Text>Library!</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: { flex: 1, justifyContent: "center", alignItems: "center" },
+});
 
 export default LibraryScreen;
