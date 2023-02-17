1e43c4db1c1f70b6cb05f3733b9bb38ffcc5ba9b
yeongmin <0miiii@naver.com>
2023-02-17 17:02:21 +0900

    feat: add monthly, weekly calendar toggle

--- pages/CalendarPage.tsx
+++ pages/CalendarPage.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import { Button } from "react-native";
+
 import MonthlyCalendar from "../components/MonthlyCalendar";
+import WeeklyCalendar from "../components/WeeklyCalendar";
+
+const WEEKLY_CALENDAR_VIEW = "주간 캘린더 보기";
+const MONTHLY_CALENDAR_VIEW = "월간 캘린더 보기";
 
 function CalendarScreen() {
-  return <MonthlyCalendar />;
+  const [calendarToggle, setCalendarToggle] = useState(false);
+  const view = !calendarToggle ? WEEKLY_CALENDAR_VIEW : MONTHLY_CALENDAR_VIEW;
+  const calendarToggleHandler = () => {
+    setCalendarToggle(!calendarToggle);
+  };
+
+  return (
+    <>
+      {!calendarToggle && <MonthlyCalendar />}
+      {calendarToggle && <WeeklyCalendar />}
+      <Button title={view} onPress={calendarToggleHandler} />
+    </>
+  );
 }
 
 export default CalendarScreen;
